b66723fd6df0e89b549b165a76115096ee1df1b7
Austin Barr and Hisato Kawaminami <austin@>
2018-06-25 17:23:51 -0700

    added

--- palindrome.cs
+++ palindrome.cs
@@ -3,44 +3,74 @@ using System.Collections.Generic;
 
 class Palindrome
 {
-  private string[] _userInput;
+  private string _userInput;
 
-  public Palindrome (string[] word)
+  public Palindrome (string word)
   {
     _userInput = word;
   }
 
-  public void SetWord (string[] newWord)
+  public bool isPalindrome()
   {
-    _userInput = newWord;
-  }
-  public string[] GetWord()
-  {
-    return _userInput;
+    char[] reversedInputArray = _userInput.ToCharArray();
+    Array.Reverse(reversedInputArray);
+    string reversedInput = new string(reversedInputArray);
+
+    if (_userInput == reversedInput)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
   }
 
+  public bool isPalindromeDifferentWay()
+  {
+    char[] userInputArray = _userInput.ToCharArray();
+    char[] reversedInputArray = _userInput.ToCharArray();
+    Array.Reverse(reversedInputArray);
 
-public static string ReverseString(string input)
-{
-  char[] arr = input.ToCharArray();
-  Array.Reverse(arr);
-  return new string(arr);
-}
+    for(int i = 0; i < userInputArray.Length; i++)
+    {
+      if(userInputArray[i] == reversedInputArray[i])
+      {
+        continue;
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  // public string ReverseString(string input)
+  // {
+  //   char[] arr = input.ToCharArray();
+  //   Array.Reverse(arr);
+  //   return new string(arr);
+  // }
 }
 
 public class Program
 {
   public static void Main()
   {
-    Console.WriteLine("Enter a word to find out if it's a palindrome");
-    string stringWord = Console.ReadLine();
-    // char[] arrayWord = stringWord.ToCharArray();
-    // string[] splitWord = arrayWord.Split();
-    // string[] word = stringWord;
+    // Console.WriteLine("Enter a word to find out if it's a palindrome");
+    // string stringWord = Console.ReadLine();
+    // // char[] arrayWord = stringWord.ToCharArray();
+    // // string[] splitWord = arrayWord.Split();
+    // // string[] word = stringWord;
+    //
+    // Console.WriteLine(Palindrome.ReverseString(stringWord));
+    Console.WriteLine("Enter something:");
+    string response = Console.ReadLine();
 
-    Console.WriteLine(Palindrome.ReverseString(stringWord));
-
-
-
+    Palindrome pal = new Palindrome(response);
+    bool result = pal.isPalindrome();
+    Console.WriteLine("This is a palindrome: " + result);
   }
 }
